eb59116173e92b00c96ce745572bef7923c95c0e
Freddy Akpoly <fakpoly@u.rochester.edu>
2025-04-21 22:24:47 -0400

    Update register.js

--- public/js/register.js
+++ public/js/register.js
@@ -4,7 +4,7 @@ window.addEventListener("load", () => {
         const password = document.getElementById("password").value;
 
         if (!username || !password) {
-            return; // Stop execution
+            return;
         }
 
         const username_available = await check_username(username);
